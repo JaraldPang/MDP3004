04f2de40f6b58aa457fda5a5c8a8c5b8f18cf373
Lee Seng Cheong <sengcheong94@gmail.com>
2018-10-04 10:20:28 +0800

    Reorganization of OpenCV directory

--- opencv/arrow_contour.py
+++ opencv/arrow_contour.py
@@ -11,7 +11,7 @@ def main():
     cnts = cv.findContours(th, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-    img1 = cv.imread('assets/arrow_real.jpg', cv.IMREAD_UNCHANGED)
+    img1 = cv.imread('testbed/arrow_real.jpg', cv.IMREAD_UNCHANGED)
     gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
     ret1, th1 = cv.threshold(gray1, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
     cnts1 = cv.findContours(th1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -21,11 +21,14 @@ def main():
     # loop over the contours
     for (i, c) in enumerate(cnts):
         x, y, w, h = cv.boundingRect(c)
-        if (cv.matchShapes(cnts1[0], c, 1, 0.0) < 0.01):
+        if (cv.matchShapes(cnts1[0], c, 1, 0.0) < 0.02):
             cv.drawContours(img, [c], -1, (0, 255, 0), 3)
             cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    cv.imwrite('testbed/results.jpg', img)
+    #cv.imwrite('testbed/results.jpg', img)
+
+    plt.imshow(img,'gray')
+    plt.show()
 
 if __name__ == '__main__':
     main()
